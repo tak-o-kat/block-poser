809b8438e10727c117e2f4e7355d9b78f26c42fb
takokat <danobakka@gmail.com>
2023-10-28 18:12:01 -0700

    Add Header and Themeswitcher to app

--- src/Components/ThemeSwitcher.tsx
+++ src/Components/ThemeSwitcher.tsx
@@ -4,8 +4,8 @@ const initializeTheme = () => {
   let theme;
   if (typeof localStorage !== "undefined" && localStorage.getItem("theme")) {
     theme = localStorage.getItem("theme") as "light" | "dark";
-  } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
-    theme = "dark";
+  } else if (localStorage.getItem('theme') === null) {
+    theme = "light";
   } else {
     theme = "light";
   }
@@ -27,18 +27,18 @@ const ThemeSwitcher = () => {
   });
 
   return (
-    <div class='text-lg' onClick={() => switchTheme()}>
+    <div class='' onClick={() => switchTheme()}>
       <Show when={isDark()}>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" />
-        </svg>
-      </Show>
-      <Show when={!isDark()}>
-        <div class='text-lg'>
+        <div class='text-gray-100'>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
+            <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z" />
           </svg>
         </div>
+      </Show>
+      <Show when={!isDark()}>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z" />
+        </svg>
       </Show>
     </div>
   );
