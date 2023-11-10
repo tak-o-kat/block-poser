b5679c445f37fe3acf779ac971278b7148717e4e
takokat <danobakka@gmail.com>
2023-11-09 19:43:37 -0800

    Update the time and date components with error handling

--- src/components/SolidDatePicker.tsx
+++ src/components/SolidDatePicker.tsx
@@ -1,61 +1,51 @@
-import { createEffect, createSignal } from 'solid-js';
-
-import DatePicker, {
-  PickerValue,
-} from '@rnwonder/solid-date-picker';
+import { Show } from 'solid-js';
+import DatePicker, { utils } from '@rnwonder/solid-date-picker';
 
 const SolidDatePicker = (props: any) => {
-  const [date, setDate] = createSignal<PickerValue>({
-    label: '',
-    value: {},
-  });
+  const minDate = { year: 2019, month: 5, day: 11 };
+  const maxDate = utils().getToday();
 
   // tailwind CSS for datepicker 
   const dateInputClasses = "bg-white dark:bg-gray-700 outline-none h-11 !pl-11 cursor-pointer";
   const wrapperClass = "bg-gray-200 dark:bg-gray-700"
   const btnClass = "text-gray-700 dark:text-gray-100 hover:bg-gray-200 dark:hover:bg-gray-600"
 
-  const updateDate = () => {
-    debugger;
-    props.setState({
-      searchForm: {
-        fields: {
-          [props.fieldName]: date()
-        }
-      }
-    });
-  };
-
-
   return (
-    <div class="relative w-full border rounded-lg bg-gray-50 dark:bg-gray-700 border-gray-300 dark:border-gray-600">
-      <div class="absolute inset-y-0 left-0 flex items-center pl-3.5 pointer-events-none">
-        <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-          <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
-        </svg>
+    <div class="flex flex-col w-full">
+      <div class={`${props.errors.error ? '!border-red-500' : ''} relative w-full border rounded-lg bg-gray-50 dark:bg-gray-700 border-gray-300 dark:border-gray-600`}>
+        <div class="absolute inset-y-0 left-0 flex items-center pl-3.5 pointer-events-none">
+          <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+            <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
+          </svg>
+        </div>
+        <div class="mx-1 text-gray-900 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white border-solid">
+          <DatePicker
+            placeholder='Select Date'
+            value={props.state} 
+            setValue={props.setState}
+            hideOutSideDays
+            formatInputLabel="yyyy-mm-dd"
+            minDate={minDate}
+            maxDate={maxDate}
+            shouldCloseOnSelect={true}
+            weekDaysType={"single"}
+            inputClass={dateInputClasses}
+            datePickerWrapperClass={wrapperClass}
+            prevNextMonthBtnClass={btnClass}
+            monthYearTriggerBtnClass={btnClass}
+            monthYearSelectorWrapperClass={wrapperClass}
+            monthYearOptionBtnClass={btnClass}
+            weekNamesClass="text-gray-50 dark:text-gray-400"
+            daysBtnClass={btnClass}
+            currentDayBtnClass="!border-solid !border-gray-400 !dark:border-gray-600"
+          />
+        </div>
       </div>
-      <div class="mx-1 text-gray-900 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white border-solid">
-        <DatePicker
-          placeholder='Select Date'
-          value={date} 
-          setValue={setDate}
-          onChange={updateDate}
-          hideOutSideDays
-          formatInputLabel="yyyy-mm-dd"
-          shouldCloseOnSelect={true}
-          weekDaysType={"single"}
-          inputClass={dateInputClasses}
-          datePickerWrapperClass={wrapperClass}
-          prevNextMonthBtnClass={btnClass}
-          monthYearTriggerBtnClass={btnClass}
-          monthYearSelectorWrapperClass={wrapperClass}
-          monthYearOptionBtnClass={btnClass}
-          weekNamesClass="text-gray-50 dark:text-gray-400"
-          daysBtnClass={btnClass}
-          currentDayBtnClass="!border-solid !border-gray-400 !dark:border-gray-600"
-        />
-      </div>
+      <Show when={props.errors.error}>
+        <span class="p-1 text-sm text-red-600">{props.errors.msg}</span>
+      </Show>
     </div>
+    
   )
 };
 
--- src/components/SolidTimePicker.tsx
+++ src/components/SolidTimePicker.tsx
@@ -1,50 +1,36 @@
-import { createSignal, createEffect } from 'solid-js';
+import { Show } from 'solid-js';
+import { TimePicker } from '@rnwonder/solid-date-picker';
 
-import {
-  TimeValue,
-  TimePicker,
-} from '@rnwonder/solid-date-picker';
-
-const SolidTimePicker = (props) => {
-  const [time, setTime] = createSignal<TimeValue>({
-    label: '',
-    value: {},
-  });
-
+const SolidTimePicker = (props: any) => {
   const timeInputClasses = "bg-white dark:bg-gray-700 text-gray-700 dark:text-gray-100 outline-none h-11 !pl-11 cursor-pointer";
   const wrapperClass = "bg-white dark:bg-gray-700 text-gray-700 dark:text-gray-100 font-bold"
   const btnClass = "text-gray-700 dark:text-gray-100 font-bold"
 
-  createEffect(() => {
-    props.setState({
-      searchForm: {
-        fields: {
-          [props.fieldName]: time
-        }
-      }
-    });
-  });
-
   return (
-    <div class="relative w-full border rounded-lg bg-gray-50 dark:bg-gray-700 border-gray-300 dark:border-gray-600 bg">
-      <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-        <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
-          <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zM12.75 6a.75.75 0 00-1.5 0v6c0 .414.336.75.75.75h4.5a.75.75 0 000-1.5h-3.75V6z" clip-rule="evenodd" />
-        </svg>
+    <div class="flex flex-col w-full">
+      <div class={`${props.errors.error ? '!border-red-500' : ''} relative w-full border rounded-lg bg-gray-50 dark:bg-gray-700 border-gray-300 dark:border-gray-600 bg`}>
+        <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+          <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+            <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zM12.75 6a.75.75 0 00-1.5 0v6c0 .414.336.75.75.75h4.5a.75.75 0 000-1.5h-3.75V6z" clip-rule="evenodd" />
+          </svg>
+        </div>
+        <div class="mx-1 text-gray-900 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white border-solid">
+          <TimePicker
+            placeholder="Select Time"
+            value={props.state} 
+            setValue={props.setState}
+            inputClass={timeInputClasses}
+            timePickerWrapperClass={wrapperClass}
+            prevNextTimeViewBtnClass={btnClass}
+            timeAnalogWrapperClass="bg-gray-100 dark:bg-gray-800"
+            timePickerMeridiemBtnClass={btnClass}
+            timeAnalogNumberClass={btnClass}
+          />
+        </div>
       </div>
-      <div class="mx-1 text-gray-900 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white border-solid">
-        <TimePicker
-          placeholder="00:00 AM"
-          value={time} 
-          setValue={setTime}
-          inputClass={timeInputClasses}
-          timePickerWrapperClass={wrapperClass}
-          prevNextTimeViewBtnClass={btnClass}
-          timeAnalogWrapperClass="bg-gray-100 dark:bg-gray-800"
-          timePickerMeridiemBtnClass={btnClass}
-          timeAnalogNumberClass={btnClass}
-        />
-      </div>
+      <Show when={props.errors.error}>
+        <span class="p-1 text-sm text-red-600">{props.errors.msg}</span>
+      </Show>
     </div>
   );
 };
