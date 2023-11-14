77b52a407e861577d1a98b65d87b8419b3342b3e
takokat <danobaba@protonmail.com>
2023-11-14 15:27:15 -0800

    fix for padding on date and time fields for the svg icon

--- src/components/SolidDatePicker.tsx
+++ src/components/SolidDatePicker.tsx
@@ -7,14 +7,14 @@ const SolidDatePicker = (props: any) => {
   const maxDate = { year: year, month: month - 1, day: day };
 
   // tailwind CSS for datepicker 
-  const dateInputClasses = "bg-white dark:bg-gray-700 outline-none h-11 !pl-11 cursor-pointer";
+  const dateInputClasses = "bg-white dark:bg-gray-700 outline-none h-11 !pl-8 cursor-pointer";
   const wrapperClass = "bg-gray-200 dark:bg-gray-700"
   const btnClass = "text-gray-700 dark:text-gray-100 hover:bg-gray-200 dark:hover:bg-gray-600"
 
   return (
     <div class="flex flex-col w-full">
       <div class={`${props.errors.error ? '!border-red-500' : ''} relative w-full border rounded-lg bg-gray-50 dark:bg-gray-700 border-gray-300 dark:border-gray-600`}>
-        <div class="absolute inset-y-0 left-0 flex items-center pl-3.5 pointer-events-none">
+        <div class="absolute inset-y-0 left-0 flex items-center pl-2.5 pointer-events-none">
           <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
             <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
           </svg>
--- src/components/SolidTimePicker.tsx
+++ src/components/SolidTimePicker.tsx
@@ -2,14 +2,14 @@ import { Show } from 'solid-js';
 import { TimePicker } from '@rnwonder/solid-date-picker';
 
 const SolidTimePicker = (props: any) => {
-  const timeInputClasses = "bg-white dark:bg-gray-700 text-gray-700 dark:text-gray-100 outline-none h-11 !pl-11 cursor-pointer";
+  const timeInputClasses = "bg-white dark:bg-gray-700 text-gray-700 dark:text-gray-100 outline-none h-11 !pl-8 cursor-pointer";
   const wrapperClass = "bg-white dark:bg-gray-700 text-gray-700 dark:text-gray-100 font-bold"
   const btnClass = "text-gray-700 dark:text-gray-100 font-bold"
 
   return (
     <div class="flex flex-col w-full">
       <div class={`${props.errors.error ? '!border-red-500' : ''} flex relative w-full border rounded-lg bg-gray-50 dark:bg-gray-700 border-gray-300 dark:border-gray-600 bg`}>
-        <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+        <div class="absolute inset-y-0 left-0 flex items-center pl-2 pointer-events-none">
           <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
             <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zM12.75 6a.75.75 0 00-1.5 0v6c0 .414.336.75.75.75h4.5a.75.75 0 000-1.5h-3.75V6z" clip-rule="evenodd" />
           </svg>
