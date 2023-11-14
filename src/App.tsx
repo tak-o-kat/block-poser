0a0139213b40585d7a5d43708f473b3aa12fb9ab
takokat <danobaba@protonmail.com>
2023-11-14 15:26:36 -0800

    fix for proper height

--- src/App.tsx
+++ src/App.tsx
@@ -13,42 +13,41 @@ const App: Component = () => {
 
 
   return (
-    <>
+    <div class="h-screen overflow-hidden bg-gray-100 dark:bg-gray-800">
       <Header />
-      <div class="flex flex-col h-screen overflow-hidden bg-gray-100 dark:bg-gray-800">
-          <BlockSearchForm />
-          <section>
-            <div class="mx-auto sm:max-w-2xl pb-5 flex justify-center text-gray-600 dark:text-gray-100">
-              <div class="text-md">
-                <div class="flow-root">
-                  <dl class="-my-3 divide-y divide-gray-300 text-sm">
-                    <div class="grid grid-cols-1 gap-1 py-3 sm:grid-cols-4 sm:gap-2">
-                      <dt class="font-medium">Node Address</dt>
-                      <dd class="sm:col-span-2">53R4HSFA24P6F7PO5LPIWH2J3UZDEYDX5O72JQPYFI2RT4VOAJRJUESSNM</dd>
-                    </div>
+      <div class="flex flex-col">
+        <BlockSearchForm />
+        <section class="flex">
+          <div class="mx-auto sm:max-w-lg pb-5 flex justify-center text-gray-600 dark:text-gray-100">
+            <div class="text-md">
+              <div class="flow-root">
+                <dl class="-my-3 divide-y divide-gray-300 text-sm">
+                  <div class="grid grid-cols-1 gap-1 py-3 sm:grid-cols-4 sm:gap-2">
+                    <dt class="font-medium">Node Address</dt>
+                    <dd class="sm:col-span-2">53R4HSFA24P6F7PO5LPIWH2J3UZDEYDX5O72JQPYFI2RT4VOAJRJUESSNM</dd>
+                  </div>
 
-                    <div class="grid grid-cols-1 gap-1 py-3 sm:grid-cols-4 sm:gap-4">
-                      <dt class="font-medium">From</dt>
-                      <dd class="sm:col-span-2">10-01-2023 00:00:00</dd>
-                    </div>
+                  <div class="grid grid-cols-1 gap-1 py-3 sm:grid-cols-4 sm:gap-4">
+                    <dt class="font-medium">From</dt>
+                    <dd class="sm:col-span-2">10-01-2023 00:00:00</dd>
+                  </div>
 
-                    <div class="grid grid-cols-1 gap-1 py-3 sm:grid-cols-4 sm:gap-4">
-                      <dt class="font-medium">Until</dt>
-                      <dd class="sm:col-span-2">10-30-2023 14:50:35</dd>
-                    </div>
+                  <div class="grid grid-cols-1 gap-1 py-3 sm:grid-cols-4 sm:gap-4">
+                    <dt class="font-medium">Until</dt>
+                    <dd class="sm:col-span-2">10-30-2023 14:50:35</dd>
+                  </div>
 
-                    <div class="grid grid-cols-1 gap-1 py-3 sm:grid-cols-4 sm:gap-4">
-                      <dt class="font-medium">Blocks Proposed</dt>
-                      <dd class="sm:col-span-2">24</dd>
-                    </div>
-                  </dl>
-                </div>
+                  <div class="grid grid-cols-1 gap-1 py-3 sm:grid-cols-4 sm:gap-4">
+                    <dt class="font-medium">Blocks Proposed</dt>
+                    <dd class="sm:col-span-2">24</dd>
+                  </div>
+                </dl>
               </div>
-
             </div>
-          </section>
+          </div>
+        </section>
       </div>
-    </>
+    </div>
   );
 };
 
