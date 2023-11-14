56523afb0dee7eb35c2f7ff27845592309315ad6
takokat <danobaba@protonmail.com>
2023-11-14 14:41:08 -0800

    add in GMT to the time fields

--- src/components/SolidTimePicker.tsx
+++ src/components/SolidTimePicker.tsx
@@ -8,7 +8,7 @@ const SolidTimePicker = (props: any) => {
 
   return (
     <div class="flex flex-col w-full">
-      <div class={`${props.errors.error ? '!border-red-500' : ''} relative w-full border rounded-lg bg-gray-50 dark:bg-gray-700 border-gray-300 dark:border-gray-600 bg`}>
+      <div class={`${props.errors.error ? '!border-red-500' : ''} flex relative w-full border rounded-lg bg-gray-50 dark:bg-gray-700 border-gray-300 dark:border-gray-600 bg`}>
         <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
           <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
             <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zM12.75 6a.75.75 0 00-1.5 0v6c0 .414.336.75.75.75h4.5a.75.75 0 000-1.5h-3.75V6z" clip-rule="evenodd" />
@@ -25,8 +25,12 @@ const SolidTimePicker = (props: any) => {
             timeAnalogWrapperClass="bg-gray-100 dark:bg-gray-800"
             timePickerMeridiemBtnClass={btnClass}
             timeAnalogNumberClass={btnClass}
-          />
+          /> 
+          
         </div>
+        <div class="flex items-center border-l dark:border-gray-600 px-1.5 sm:px-3 ">
+            GMT
+          </div>
       </div>
       <Show when={props.errors.error}>
         <span class="p-1 text-sm text-red-600">{props.errors.msg}</span>
