595a0cb0aaf0c389e61ac4e372a981c1145235f4
takokat <danobaba@protonmail.com>
2023-11-16 18:41:49 -0800

    This is actually the minor fix for timezone area in time fields

--- src/components/SolidTimePicker.tsx
+++ src/components/SolidTimePicker.tsx
@@ -8,13 +8,13 @@ const SolidTimePicker = (props: any) => {
 
   return (
     <div class="flex flex-col w-full">
-      <div class={`${props.errors.error ? '!border-red-500' : ''} flex relative w-full border rounded-lg bg-gray-50 dark:bg-gray-700 border-gray-300 dark:border-gray-600 bg`}>
+      <div class={`${props.errors.error ? '!border-red-500' : ''} flex bg-white relative w-full border rounded-lg bg-gray-50 dark:bg-gray-700 border-gray-300 dark:border-gray-600 bg`}>
         <div class="absolute inset-y-0 left-0 flex items-center pl-2 pointer-events-none">
           <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
             <path fill-rule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zM12.75 6a.75.75 0 00-1.5 0v6c0 .414.336.75.75.75h4.5a.75.75 0 000-1.5h-3.75V6z" clip-rule="evenodd" />
           </svg>
         </div>
-        <div class="mx-1 w-full text-gray-900 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white border-solid">
+        <div class="ml-1.5 w-full text-gray-900 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white border-solid">
           <TimePicker
             placeholder="Select Time"
             value={props.state} 
@@ -28,7 +28,7 @@ const SolidTimePicker = (props: any) => {
           /> 
           
         </div>
-        <div class="flex mx-auto items-center border-l dark:border-gray-600 sm:px-3 px-1.5">
+        <div class="flex items-center bg-white dark:bg-gray-700 border-l dark:border-gray-600 pl-1.5 pr-0.5 sm:pl-3 sm:pr-1.5 mr-1.5">
             GMT
           </div>
       </div>
