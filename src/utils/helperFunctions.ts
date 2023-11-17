6d2add36d964cdf510d7844ebe316224e3f46a09
takokat <danobaba@protonmail.com>
2023-11-17 14:00:45 -0800

    Add helper function to convert 24 hours to 12 hours

--- src/utils/helperFunctions.ts
+++ src/utils/helperFunctions.ts
@@ -15,3 +15,12 @@ export const convertTime12to24 = (time12h: string) => {
 
   return `${hours}:${minutes}:${seconds === undefined ? '00': seconds}`;
 }
+
+export const convertTime24to12 = (time24h: string) => {
+  let [hours, minutes, seconds] = time24h.split(':');
+  const modifier = (hours > '11' && hours < '24') ? 'PM' : 'AM'
+
+  // convert hour to appropriate 12 hour digit
+  hours = (parseInt(hours) % 12 === 0) ? '12' : (parseInt(hours) % 12).toString();
+  return `${hours.length === 1 ? '0' + hours : hours}:${minutes}:${seconds} ${modifier}`;
+}
