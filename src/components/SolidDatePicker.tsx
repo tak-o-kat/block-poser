7c564dbeeead8f495f5de3e572fa426faaeb9166
takokat <danobaba@protonmail.com>
2023-11-16 18:54:39 -0800

    Fix for minor border isssues with solid date picker

--- src/components/SolidDatePicker.tsx
+++ src/components/SolidDatePicker.tsx
@@ -13,13 +13,13 @@ const SolidDatePicker = (props: any) => {
 
   return (
     <div class="flex flex-col w-full">
-      <div class={`${props.errors.error ? '!border-red-500' : ''} relative w-full border rounded-lg bg-gray-50 dark:bg-gray-700 border-gray-300 dark:border-gray-600`}>
+      <div class={`${props.errors.error ? '!border-red-500' : ''} bg-white relative w-full border rounded-lg bg-gray-50 dark:bg-gray-700 border-gray-300 dark:border-gray-600`}>
         <div class="absolute inset-y-0 left-0 flex items-center pl-2.5 pointer-events-none">
           <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
             <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
           </svg>
         </div>
-        <div class="mx-1 text-gray-900 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white border-solid">
+        <div class="mx-1.5 text-gray-900 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white border-solid">
           <DatePicker
             placeholder='Select Date'
             value={props.state} 
