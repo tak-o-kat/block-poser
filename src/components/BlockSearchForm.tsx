bc37e1f491096f7ebed6dc4dad6ee33dac614ac2
takokat <danobaba@protonmail.com>
2023-12-10 07:39:05 -0800

    Remove the dump button and state

--- src/components/BlockSearchForm.tsx
+++ src/components/BlockSearchForm.tsx
@@ -64,7 +64,6 @@ const BlockSearchForm = () => {
       endDate: '',
       endTime: '',
       getList: false,
-      dump: false
     },
     errors: {
       accountAddress: {
@@ -277,31 +276,7 @@ const BlockSearchForm = () => {
               >
                 {searching() ? 'Seaching...' : 'Search'}
               </button>
-             
             </div>
-            <div class="flex items-center justify-between">
-              <button
-                type="button"
-                class={`inline-block w-full rounded-lg !bg-blue-400 dark:!bg-blue-500 px-5 py-3 font-medium text-white sm:w-[12rem]`}
-                onclick={() => {
-                  setFormState({
-                    ...formState,
-                    fields: {
-                      ...formState.fields,
-                      dump: !formState.fields.dump
-                    }
-                  })
-                }}
-              >
-                Dump
-              </button>
-            </div>
-            <Show when={formState.fields.dump}>
-              {`type: ${window.performance.getEntriesByType("navigation")[0]?.type}`}
-              <pre>
-                {`${JSON.stringify(restoreLocalData(), null, 2)}`}
-              </pre>
-            </Show>
           </fieldset>
         </form>
       </div>
