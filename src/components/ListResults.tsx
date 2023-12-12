ed47d75470d7f59bdc0b6b5c7a8e699d9bfc5dbe
takokat <danobaba@protonmail.com>
2023-12-12 11:11:21 -0800

    Add in algoexplorer link to each block in the list view

--- src/components/ListResults.tsx
+++ src/components/ListResults.tsx
@@ -9,14 +9,33 @@ const ListResults = () => {
       <Show when={store.state.results.getList}>
         <div class="mx-auto sm:max-w-3xl block items-center justify-center h-full text-gray-600 dark:text-gray-400 p-3">
           <div class="flex flex-row justify-center w-full">
-            <div class="basis-1/3 flex justify-center border-b border-r border-gray-300 dark:border-gray-600 font-semibold">Block</div>
-            <div class="basis-2/3 flex justify-center border-b border-gray-300 dark:border-gray-600 font-semibold">DateTime</div>
+            <div class="basis-1/3 flex justify-center border-b border-r border-gray-300 dark:border-gray-600 font-semibold">
+              Block
+            </div>
+            <div class="basis-2/3 flex justify-center border-b border-gray-300 dark:border-gray-600 font-semibold">
+              DateTime
+            </div>
           </div>
-          <For each={store.state.results.blockList} fallback={<div class="basis-1/3 flex py-2 justify-center">No Data</div>}>
+          <For
+            each={store.state.results.blockList}
+            fallback={
+              <div class="basis-1/3 flex py-2 justify-center">No Data</div>
+            }
+          >
             {(item, index) => (
               <div class="flex py-1">
-                <div class="basis-1/3 flex justify-center">{item?.round}</div>
-                <div class="basis-2/3 flex justify-center">{item?.realtime}</div>
+                <div class="basis-1/3 flex justify-center">
+                  <a
+                    href={`https://algoexplorer.io/block/${item?.round}`}
+                    target="_blank"
+                    class="underline"
+                  >
+                    {item?.round}
+                  </a>
+                </div>
+                <div class="basis-2/3 flex justify-center">
+                  {item?.realtime}
+                </div>
               </div>
             )}
           </For>
