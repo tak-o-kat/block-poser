9228f4e39977fc95138e4443d5f012e736796c61
takokat <danobaba@protonmail.com>
2023-12-12 11:28:07 -0800

    Adjust font size and spacing for list view

--- src/components/ListResults.tsx
+++ src/components/ListResults.tsx
@@ -8,7 +8,7 @@ const ListResults = () => {
     <section class="mx-auto w-full p-4 bg-gray-100 dark:bg-gray-800">
       <Show when={store.state.results.getList}>
         <div class="mx-auto sm:max-w-3xl block items-center justify-center h-full text-gray-600 dark:text-gray-400 p-3">
-          <div class="flex flex-row justify-center w-full">
+          <div class="flex flex-row justify-center w-full py-1.5 text-lg">
             <div class="basis-1/3 flex justify-center border-b border-r border-gray-300 dark:border-gray-600 font-semibold">
               Block
             </div>
@@ -23,7 +23,7 @@ const ListResults = () => {
             }
           >
             {(item, index) => (
-              <div class="flex py-1">
+              <div class="flex py-1.5 text-lg">
                 <div class="basis-1/3 flex justify-center">
                   <a
                     href={`https://algoexplorer.io/block/${item?.round}`}
