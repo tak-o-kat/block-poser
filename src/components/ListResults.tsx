21b7093b1ac503af642fef5493cc37b75731980c
takokat <danobaba@protonmail.com>
2023-12-24 10:52:18 -0800

    Updated ListResult Show with the correct logic and moved the No Blocks result back to the center.

--- src/components/ListResults.tsx
+++ src/components/ListResults.tsx
@@ -1,13 +1,13 @@
 import { For, Show } from 'solid-js';
 import { useGlobalContext } from '../context/store';
-import { SkeletonBlockResult, SkeletonListResult } from './SkeletonLoaders';
+import { SkeletonListResult } from './SkeletonLoaders';
 
 const ListResults = () => {
   const store: any = useGlobalContext();
 
   return (
     <section class="mx-auto w-full p-4 bg-gray-100 dark:bg-gray-800">
-      <Show when={true}>
+      <Show when={store.state.results.getList}>
         <div class="mx-auto sm:max-w-3xl block items-center justify-center h-full text-gray-600 dark:text-gray-400 p-3">
           <div class="flex flex-row justify-center w-full py-1.5 text-lg">
             <div class="basis-1/3 flex justify-center border-b border-r border-gray-300 dark:border-gray-600 font-semibold">
@@ -17,11 +17,14 @@ const ListResults = () => {
               DateTime
             </div>
           </div>
-          <Show when={!store.state.results.isLoading} fallback={<SkeletonListResult />}>
+          <Show 
+            when={!store.state.results.isLoading} 
+            fallback={<SkeletonListResult />}
+          >
             <For
               each={store.state.results.blockList}
               fallback={
-                <div class="basis-1/3 flex py-2 justify-start">No Blocks</div>
+                <div class="basis-2/3 flex py-2 justify-center">No Blocks</div>
               }
             >
               {(item) => (
